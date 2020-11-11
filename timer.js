34cbfae11f07dfdad9d8fc678448f725ce81a744
anniebart <anniebartholomew@gmail.com>
2020-11-11 16:21:48 -0500

    reworked timer + UI

--- timer.js
+++ timer.js
@@ -1,44 +1,48 @@
 const s = document.querySelector('.additional-min');
 const time = document.querySelector('.time');
-const circle = document.querySelector('.circle')
+const circle = document.querySelector('.circle');
+let input = document.querySelector('.minutes');
 let count = 0
 
-
-
-
-window.addEventListener('load', focus());
-window.addEventListener('mousemove', ()=>{
-    resetFocus()
-    time.innerText = '0';
-})
-
-  function focus(){
-
-    
-    setInterval(()=>{
-        count++
+input.addEventListener('change', ()=>{
+    count = input.value;
+    time.innerText = count
+    if (count == 1){
+        s.style.opacity = '0'  
         console.log(count)
-        time.innerText = count;
-        if (count ===1){
-            s.style.opacity = '0'
-        } else {
-            s.style.opacity = '1'
+    }
+    console.log(count)
+    
+        setInterval(()=>{
+           if (count> 0) {count --;
+            console.log(count);
+            time.innerText = count;
+            s.style.opacity = '1'  
+            if (count ===1){
+                s.style.opacity = '0'  
+            } 
         }
-    }, 60000)
-   
+             
+            else if(count ===0){
+                s.style.opacity = '1'  
+                circle.innerHTML="you're done!"
+                
+            }
+            
+        }, 60000)
+    
 
+});
+
+
+
+window.addEventListener('mousemove', () =>{
+
+      count = input.value || 0
+      console.log(count)
+      time.innerText= count
      
   }
-
-  function resetFocus(){
-      console.log('reset focus!')
-      count = 0
-}
+);
 
 
-function updateTime(){
-    
-    time.innerText = count;
-  
-    console.log(count)
-}
