93c24760f24cb076ebe54b0d5c5c1cc054154093
anniebart <anniebartholomew@gmail.com>
2020-11-13 11:27:43 -0500

    styled UI

--- timer.js
+++ timer.js
@@ -4,47 +4,54 @@ const circle = document.querySelector('.circle');
 let input = document.querySelector('.minutes');
 let count = 0
 
+
 circle.style.webkitAnimationPlayState = "paused";
 
 input.addEventListener('change', ()=>{
+    circle.innerText= 'just breathe'
     count = input.value;
     circle.style.webkitAnimationPlayState = "running";
     time.innerText = count
     if (count == 1){
         s.style.opacity = '0'  
-        console.log(count)
-    }
-    console.log(count)
-    
-        setInterval(()=>{
-           if (count> 0) {count --;
-            console.log(count);
+    }    
+
+    if (count> 0) {
+       
+    const startTimer = setInterval(()=>{
+            
+            count --;
+        
             time.innerText = count;
             s.style.opacity = '1'  
+
             if (count ===1){
                 s.style.opacity = '0'  
             } 
-        }
              
-            else if(count ===0){
+             if(count ===0){
                 s.style.opacity = '1'  
                 circle.innerHTML="you're done!"
-                
+                circle.style.webkitAnimationPlayState = "paused";
+                clearInterval(startTimer)      
             }
             
         }, 60000)
-    
+    }
 
 });
 
 
 
 window.addEventListener('mousemove', () =>{
-
+    if (count > 1){
       count = input.value || 0
       console.log(count)
       time.innerText= count
-     
+    } else {
+        time.innerText= '0';
+        
+    }
   }
 );
 
